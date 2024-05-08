f74121d84f908c9862dcf41ac06b0c029ba18887
Alvin Johansson <alvin.johansson@elva-group.com>
2024-05-08 17:14:49 +0200

    feat: add sst

---
+++ sst.config.ts
@@ -0,0 +1,32 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "lagom-eurovision",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      providers: {
+        aws: {
+          region: "eu-north-1",
+        },
+      },
+    };
+  },
+  async run() {
+    const site = new sst.aws.StaticSite("Euro", {
+      environment: {
+        VITE_REFLECT_URL: "https://lagom-euro-paliago.reflect-server.net",
+      },
+      build: {
+        command: "npm run build",
+        output: "dist",
+      },
+      domain: "lagomeuro.com",
+    });
+
+    return {
+      url: site.url,
+    };
+  },
+});
